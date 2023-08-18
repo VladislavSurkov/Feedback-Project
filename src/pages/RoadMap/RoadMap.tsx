a89fd6daf5ee5e49f4e07e4e65be59a1840875bf
VladislavSurkov <fvhabjrega@gmail.com>
2023-08-18 19:53:56 +0300

    Update RoadMap.tsx

--- src/pages/RoadMap/RoadMap.tsx
+++ src/pages/RoadMap/RoadMap.tsx
@@ -1,8 +1,14 @@
-
+import { ProductList } from "components/ProductList/ProductList";
+import { useTypedSelector } from "hooks/useHooks";
 
 const RoadMap = () => {
-    return (
-        <div>hello</div>
-    )
-}
-export default RoadMap
+  const { products, isLoading } = useTypedSelector(state => state.todo);
+
+  return (
+    <>
+      <div>RoadMap</div>
+      {isLoading ? <div>Loading...</div> : <ProductList products={products} />}
+    </>
+  );
+};
+export default RoadMap;
