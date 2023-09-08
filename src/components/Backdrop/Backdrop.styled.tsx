e982cd8552069e4dfe878c5d7d52d89c5b011b94
VladislavSurkov <fvhabjrega@gmail.com>
2023-09-08 14:13:12 +0300

    Update Modal css

--- src/components/Backdrop/Backdrop.styled.tsx
+++ src/components/Backdrop/Backdrop.styled.tsx
@@ -9,22 +9,18 @@ export const Backdrop = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: #fff;
+  background-color: #f7f8fd;
 `;
 
 export const ModalContainer = styled.div`
-  position: relative;
   min-width: 350px;
   min-height: 800px;
-  padding: 88px 24px 77px 24px;
-  background-color: #f2f2f2;
-  border-radius: 16px;
+  padding: 34px 24px;
 `;
 
 export const BtnGoBack = styled.button`
-  position: absolute;
-  top: 50px;
   color: #647196;
   border: none;
   background: none;
+  margin-bottom: 55px;
 `;
--- src/components/ModalAddFeedback/ModalAddFeedback.styled.tsx
+++ src/components/ModalAddFeedback/ModalAddFeedback.styled.tsx
@@ -4,7 +4,8 @@ import { ReactComponent as PlusIcon } from 'icons/icon-new-feedback.svg';
 export const StyledPlusIcon = styled(PlusIcon)`
   position: absolute;
   top: 0;
-  left:28px;
+  left: 24px;
+  transform: translateY(-50%);
 `;
 
 export const Box = styled.div`
